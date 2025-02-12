12fe5bfac3a1f74b55837eed87c3f590bb6bff13
Kurt Ferreira <kurt@reload.co.za>
2025-02-12 11:54:23 +0200

    Update stb_cvar.h
    Adding flags and min, max. Untested, I raw dogged this into commit via Github.

--- stb_cvar.h
+++ stb_cvar.h
@@ -10,6 +10,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <float.h> // for double min and max
 
 #ifndef CVAR_MAX_NAME
   #define CVAR_MAX_NAME 128
@@ -30,6 +31,12 @@
 extern "C" {
 #endif
 
+// CVAR options
+#define CVAR_OPT_SAVE       (1<<1)   // Save the CVAR to a file (this is up to the implementor)
+#define CVAR_OPT_RESTART    (1<<2)   // Changing this Cvar will result in a restart of some systems (again, up to the implementor)
+#define CVAR_OPT_VISIBLE    (1<<3)   // The cvar is a menu-visible setting
+// Define your own options over this point starting with (1<<4)
+
 enum {
   CVAR_NUMBER = 0,
   CVAR_STRING = 1,
@@ -44,17 +51,29 @@ typedef struct {
     double number;
     bool boolean;
   } value;
+  double min;
+  double max;
+  bool must_restart;
+  unsigned int flags;
+
 } cvar_t;
 
 //
 // Declarations
 //
 
-cvar_t* stb_cvar_create(const char* name, int type);
+cvar_t* stb_cvar_create(const char* name, int type, unsigned int flags);
 void stb_cvar_destroy(cvar_t* cvar);
 void stb_cvar_set_number(cvar_t* cvar, double value);
 void stb_cvar_set_str(cvar_t* cvar, char* value);
 void stb_cvar_set_bool(cvar_t* cvar, bool value);
+
+void stb_cvar_set_min(cvar_t* cvar, double value);
+void stb_cvar_set_max(cvar_t* cvar, double value);
+void stb_cvar_set_opt(cvar_t* cvar, unsigned int option);
+void stb_cvar_clear_opt(cvar_t* cvar, unsigned int option);
+bool stb_cvar_has_opt(cvar_t* cvar, unsigned int option);
+
 const char* stb_cvar_get_str(cvar_t* cvar);
 double stb_cvar_get_number(cvar_t* cvar);
 bool stb_cvar_get_bool(cvar_t* cvar);
@@ -73,12 +92,16 @@ bool stb_cvar_get_bool(cvar_t* cvar);
 // Implementation
 //
 
-cvar_t* stb_cvar_create(const char* name, int type) {
+cvar_t* stb_cvar_create(const char* name, int type, unsigned int flags) {
   CVAR_ASSERT(strlen(name) < CVAR_MAX_NAME);
   
   cvar_t* cvar = CVAR_ALLOC(sizeof(cvar_t));
   CVAR_ASSERT(cvar != NULL);
   cvar->type = type;
+   cvar->must_restart = false;
+   cvar->flags = flags;
+   cvar->min = DBL_MIN;
+   cvar->max = DBL_MAX;
   strcpy(cvar->name, name);
    
   return cvar;
@@ -96,12 +119,39 @@ void stb_cvar_set_str(cvar_t* cvar, char* value) {
 
 void stb_cvar_set_number(cvar_t* cvar, double value) {
   CVAR_ASSERT(cvar);
+  CVAR_ASSERT(value > cvar->min && value < cvar->max); // if the user specifies bounds, otherwise we'll simply wrap around
+   
   cvar->value.number = value;
 }
 
 void stb_cvar_set_bool(cvar_t* cvar, bool value) {
   CVAR_ASSERT(cvar);
   cvar->value.boolean = value;
+}
+
+void stb_cvar_set_min(cvar_t* cvar, double value) {
+   CVAR_ASSERT(cvar);
+   cvar->min = value;
+}
+
+void stb_cvar_set_max(cvar_t* cvar, double value) {
+   CVAR_ASSERT(cvar);
+   cvar->max = value;
+}
+
+void stb_cvar_set_opt(cvar_t* cvar, unsigned int option) {
+   CVAR_ASSERT(cvar);
+   cvar->flags |= option;
+}
+
+void stb_cvar_clear_opt(cvar_t* cvar, unsigned int option) { 
+   CVAR_ASSERT(cvar);
+   cvar->flags ~= option;
+}
+
+bool stb_cvar_has_opt(cvar_t* cvar, unsigned int option) {
+   CVAR_ASSERT(cvar);
+   return cvar->flags & option;
 }
 
 const char* stb_cvar_get_str(cvar_t* cvar) {
